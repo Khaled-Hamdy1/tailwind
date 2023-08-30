15bf76e806c5bf415c83fcbaa0cc2b3d69512b94
Khaled Hamdy <khaledhamdy609@gmail.com>
2023-08-30 22:37:41 +0300

    add search functionallity and fix some design & logic issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,10 @@
 const photos = document.querySelector("main");
-let url = "https://api.pexels.com/v1/curated?per_page=30";
+let home = true;
+let Home_URL = "https://api.pexels.com/v1/curated?per_page=30";
+let search_URL = "https://api.pexels.com/v1/search";
+let isReachedToTheEnd = false;
 
-async function renderData() {
+async function renderData(url) {
   const options = {
     method: "GET",
     headers: {
@@ -34,23 +37,43 @@ async function renderData() {
       `;
     });
     photos.innerHTML += str;
-    console.log(window.scrollY);
+    if (url === Home_URL) Home_URL = result.next_page;
+    else search_URL = result.next_page;
     url = result.next_page;
+    console.log(search_URL);
   } catch (error) {
     console.error(error);
+  } finally {
+    isReachedToTheEnd = false;
   }
 }
-window.addEventListener("load", () =>{photos.innerHTML=""; renderData()});
+window.addEventListener("load", () => {
+  photos.innerHTML = "";
+  renderData(Home_URL);
+});
 window.addEventListener("scroll", () => {
-  setTimeout(() => {
-    const totalContentHeight = document.body.scrollHeight; // Total height of the content
-    const viewportHeight = window.innerHeight; // Height of the viewport
-    const scrollPosition = window.scrollY; // Current vertical scroll position
-    const distanceFromBottom =
-      totalContentHeight - (scrollPosition + viewportHeight);
+  if (isReachedToTheEnd) return;
+  const totalContentHeight = document.body.scrollHeight; // Total height of the content
+  const viewportHeight = window.innerHeight; // Height of the viewport
+  const scrollPosition = window.scrollY; // Current vertical scroll position
+  const distanceFromBottom =
+    totalContentHeight - (scrollPosition + viewportHeight);
 
-    if (distanceFromBottom <= 20) {
-      renderData();
-    }
-  }, 3000);
+  if (distanceFromBottom <= 20) {
+    renderData(home ? Home_URL : search_URL);
+    isReachedToTheEnd = true;
+  }
+});
+//////////////////Search//////////////////////
+const searchInput = document.querySelector("input");
+const searchBtn = document.querySelector("#form-submit");
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  home = false;
+  if (search_URL === "https://api.pexels.com/v1/search") {
+    photos.innerHTML = "";
+    renderData(`${search_URL}?query=${searchInput.value}&per_page=2`);
+  } else renderData(search_URL);
+  console.log(searchInput.value);
 });
