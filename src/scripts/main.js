bfdec75b8ea3024e7ad9193f4c946f8891667682
Khaled Hamdy <khaledhamdy609@gmail.com>
2023-08-30 13:00:35 +0300

    fixing the bug of image size

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 const photos = document.querySelector("main");
+let url = "https://api.pexels.com/v1/curated?per_page=30";
 
 async function renderData() {
-  const url = "https://api.pexels.com/v1/curated?per_page=15";
   const options = {
     method: "GET",
     headers: {
@@ -14,30 +14,43 @@ async function renderData() {
     let str = "";
     result.photos.forEach((photo) => {
       str += `
-      <div class="relative m-4">
+      <div class="relative m-4 h-fit rounded-lg shadow-xl">
       <img  
         loading="lazy"
         src="${photo.src.original}"
         srcset="
-          ${photo.src.small}   150w,
-          ${photo.src.tiny}   300w,
-          ${photo.src.medium}   400w,
-          ${photo.src.landscape}  600w,
-          ${photo.src.medium}   800w,
-          ${photo.src.medium} 1200w,
-          ${photo.src.large} 1600w
-        "
+            ${photo.src.original}?auto=compress&amp;cs=tinysrgb&amp;w=150&amp;lazy=load   150w,
+            ${photo.src.original}?auto=compress&amp;cs=tinysrgb&amp;w=300&amp;lazy=load   300w,
+            ${photo.src.original}?auto=compress&amp;cs=tinysrgb&amp;w=400&amp;lazy=load   400w,
+            ${photo.src.original}?auto=compress&amp;cs=tinysrgb&amp;w=600&amp;lazy=load   600w,
+            ${photo.src.xl}?auto=compress&amp;cs=tinysrgb&amp;w=800&amp;lazy=load   800w,
+            ${photo.src.original}?auto=compress&amp;cs=tinysrgb&amp;w=1200&amp;lazy=load 1200w,
+          "
+          sizes="(max-width: 250px) calc((100vw - 30px - 15px) / 2), (max-width: 900px) calc((100vw - 30px - 15px) / 3),calc((1600px - 160px - 60px) / 4)"
         alt="${photo.alt}"
-        class="object-cover"
+        class="object-cover rounded-lg"
       />
     </div>
       `;
     });
-    photos.innerHTML = str;
-    // console.log(JSON.stringify(result,null,1));
+    photos.innerHTML += str;
+    console.log(window.scrollY);
+    url = result.next_page;
   } catch (error) {
     console.error(error);
   }
 }
+window.addEventListener("load", () =>{photos.innerHTML=""; renderData()});
+window.addEventListener("scroll", () => {
+  setTimeout(() => {
+    const totalContentHeight = document.body.scrollHeight; // Total height of the content
+    const viewportHeight = window.innerHeight; // Height of the viewport
+    const scrollPosition = window.scrollY; // Current vertical scroll position
+    const distanceFromBottom =
+      totalContentHeight - (scrollPosition + viewportHeight);
 
-renderData();
+    if (distanceFromBottom <= 20) {
+      renderData();
+    }
+  }, 3000);
+});
